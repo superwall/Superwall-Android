e27611c87e3dc92842503071b699d0a354af4e73
Bryan Dubno <bryan@superwall.com>
2023-07-20 11:07:48 -0600

    UI test 0 recorded

--- app/src/androidTest/java/com/example/superapp/UITests.kt
+++ app/src/androidTest/java/com/example/superapp/UITests.kt
@@ -6,26 +6,14 @@ import com.superwall.superapp.MainActivity
 import org.junit.Test
 
 class UITests: ScreenshotTest {
-//    @Test
-//    fun theActivityIsShownProperly() {
-//        val mainActivity = startMainActivity();
-//        /*
-//          * Take the actual screenshot. At the end of this call, the screenshot
-//          * is stored on the device and the gradle plugin takes care of
-//          * pulling it and displaying it to you in nice ways.
-//          */
-//        compareScreenshot(activity);
-//    }
-
     @Test
-    fun theActivityIsShownProperly() {
-        // Launch the activity
-        ActivityScenario.launch(MainActivity::class.java).use { scenario ->
-            // After launching, we take a screenshot
-            scenario.onActivity { activity ->
-                compareScreenshot(activity)
-            }
+    fun test0() {
+        val scenario = ActivityScenario.launch(MainActivity::class.java)
+        scenario.onActivity { activity ->
+            compareScreenshot(activity, name = "test0")
         }
     }
+
+
 }
 
