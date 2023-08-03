781e29ac7be113d642fabc151e7ca4ce8e269e06
Bryan Dubno <bryan@superwall.com>
2023-08-03 09:21:40 -0700

    Move to setup func

--- app/src/androidTest/java/com/example/superapp/UITests.kt
+++ app/src/androidTest/java/com/example/superapp/UITests.kt
@@ -4,31 +4,48 @@ import androidx.test.core.app.ActivityScenario
 import com.karumi.shot.ScreenshotTest
 import com.superwall.sdk.Superwall
 import com.superwall.sdk.delegate.SubscriptionStatus
+import com.superwall.sdk.identity.identify
 import com.superwall.sdk.identity.setUserAttributes
 import com.superwall.superapp.MainActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import org.junit.Before
 
 class UITests: ScreenshotTest {
-    @Test
-    fun test0() {
+    @Before
+    fun setup() {
         val scenario = ActivityScenario.launch(MainActivity::class.java)
         scenario.onActivity { activity ->
+            Superwall.configure(activity, "pk_5f6d9ae96b889bc2c36ca0f2368de2c4c3d5f6119aacd3d2")
 
+            // TODO: Fix this so we don't need to make the user set this
+            Superwall.instance.setSubscriptionStatus(SubscriptionStatus.Inactive)
+        }
+    }
+
+    /// Uses the identify function. Should see the name 'Jack' in the paywall.
+    @Test fun test0() {
+        val scenario = ActivityScenario.launch(MainActivity::class.java)
+        scenario.onActivity { activity ->
+            // Test 0
+            // TODO: Identify causes a crash
+//            Superwall.instance.identify("test0")
+            Superwall.instance.setUserAttributes(mapOf("first_name" to "Jack"))
+            Superwall.instance.register("present_data")
+
+            // Delay before taking screenshot
+            runBlocking {
+                delay(10000)
+            }
 
             compareScreenshot(activity, name = "test0")
         }
-
-        // UI-Tests
-        Superwall.configure(this, "pk_5f6d9ae96b889bc2c36ca0f2368de2c4c3d5f6119aacd3d2")
-
-        // TODO: Fix this so we don't need to make the user set this
-        Superwall.instance.setSubscriptionStatus(SubscriptionStatus.Inactive)
-
-        // Test 0
-        Superwall.instance.setUserAttributes(mapOf("first_name" to "Jack"))
-        Superwall.instance.register("present_data")
     }
+}
 
+// Extensions for UITests.kt
+fun UITests.compareValue(value: String, name: String) {
 
 }
 
--- app/src/main/java/com/superwall/superapp/MainApplication.kt
+++ app/src/main/java/com/superwall/superapp/MainApplication.kt
@@ -17,5 +17,15 @@ class MainApplication : android.app.Application() {
 //        // TODO: Fix this so we don't need to make the user set this
 //        Superwall.instance.setSubscriptionStatus(SubscriptionStatus.Inactive)
 //        Superwall.instance.register("campaign_trigger", mapOf("test_key" to "test_value"))
+
+//        // UI-Tests
+//        Superwall.configure(this, "pk_5f6d9ae96b889bc2c36ca0f2368de2c4c3d5f6119aacd3d2")
+//
+//        // TODO: Fix this so we don't need to make the user set this
+//        Superwall.instance.setSubscriptionStatus(SubscriptionStatus.Inactive)
+//
+//        // Test 0
+//        Superwall.instance.setUserAttributes(mapOf("first_name" to "Jack"))
+//        Superwall.instance.register("present_data")
     }
 }
