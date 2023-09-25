40673c2da588503474bf823718175f335de62aa2
Bryan Dubno <bryan@superwall.com>
2023-09-25 11:07:37 -0700

    Jetpack compose setup, cleanup

--- app/src/main/java/com/superwall/superapp/ComposeActivity.kt
+++ app/src/main/java/com/superwall/superapp/ComposeActivity.kt
@@ -8,12 +8,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -25,24 +30,58 @@ class ComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Box(modifier = Modifier.fillMaxSize()) {
-                        Column(
-                            modifier = Modifier.align(Alignment.Center),
-                            verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Greeting("Jetpack Compose")
-                            EventButton()
-                        }
+            ComposeActivityContent()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeActivityContent() {
+    val selectedTabIndex = remember { mutableStateOf(0) }
+
+    MyApplicationTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column {
+                TabRow(selectedTabIndex = selectedTabIndex.value) {
+                    Tab(
+                        selected = selectedTabIndex.value == 0,
+                        onClick = { selectedTabIndex.value = 0 }
+                    ) {
+                        Text(text = "First Tab", modifier = Modifier.padding(16.dp))
+                    }
+                    Tab(
+                        selected = selectedTabIndex.value == 1,
+                        onClick = { selectedTabIndex.value = 1 }
+                    ) {
+                        Text(text = "Second Tab", modifier = Modifier.padding(16.dp))
+                    }
+                }
+
+                Box(modifier = Modifier.fillMaxSize()) {
+                    when (selectedTabIndex.value) {
+                        0 -> TabContent1()
+                        1 -> TabContent2()
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun TabContent1() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Greeting("Jetpack Compose")
+            EventButton()
         }
     }
 }
@@ -69,10 +108,8 @@ fun EventButton() {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
+fun TabContent2() {
+    // Place your second tab content here
+    Text("This is the second tab content.")
 }
