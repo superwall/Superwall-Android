979859331070860eb314e2400abeae687e44ddb1
Yusuf Tör <yusuf@yusuftor.co.uk>
2023-09-25 13:38:43 +0800

    Added diff API keys in a comment

--- app/src/main/java/com/superwall/superapp/MainApplication.kt
+++ app/src/main/java/com/superwall/superapp/MainApplication.kt
@@ -149,7 +149,19 @@ class MainApplication : android.app.Application(), SuperwallDelegate {
 
         val purchaseController =  PurchaseControllerImpl(this)
 
-        Superwall.configure(this, "pk_5f6d9ae96b889bc2c36ca0f2368de2c4c3d5f6119aacd3d2", purchaseController)
+
+        /*
+        Copy and paste the following API keys to switch between apps.
+        App API Keys:
+            Android Tests: pk_d1f0959f70c761b1d55bb774a03e22b2b6ed290ce6561f85
+            UITest (default): pk_5f6d9ae96b889bc2c36ca0f2368de2c4c3d5f6119aacd3d2
+         */
+
+        Superwall.configure(
+            this,
+            "pk_d1f0959f70c761b1d55bb774a03e22b2b6ed290ce6561f85",
+            purchaseController
+        )
         Superwall.instance.delegate = this
 //
 //        // TODO: Fix this so we don't need to make the user set this
