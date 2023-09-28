51dc6cbd97c37d4eb9791a5b86304bd4dbec0faf
Brian Anglin <brianranglin@gmail.com>
2023-09-27 22:34:12 -0400

    Adds composable to main sdk package

---
+++ superwall/src/main/java/com/superwall/sdk/composable/PaywallComposable.kt
@@ -0,0 +1,76 @@
+package com.superwall.sdk.composable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.superwall.sdk.Superwall
+import com.superwall.sdk.paywall.presentation.get_paywall.getPaywall
+import com.superwall.sdk.paywall.presentation.internal.request.PaywallOverrides
+import com.superwall.sdk.paywall.vc.PaywallViewController
+import com.superwall.sdk.paywall.vc.delegate.PaywallViewControllerDelegate
+
+@Composable
+fun PaywallComposable(
+    event: String,
+    params: Map<String, Any>? = null,
+    paywallOverrides: PaywallOverrides? = null,
+    delegate: PaywallViewControllerDelegate,
+    errorComposable: @Composable ((Throwable) -> Unit) = { error: Throwable ->
+        // Default error composable
+        Text(text = "No paywall to display")
+    },
+    loadingComposable: @Composable (() -> Unit) = {
+        // Default loading composable
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier.align(Alignment.Center),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+    }
+) {
+    val viewState = remember { mutableStateOf<PaywallViewController?>(null) }
+    val errorState = remember { mutableStateOf<Throwable?>(null) }
+
+    LaunchedEffect(Unit) {
+        try {
+            val newView = Superwall.instance.getPaywall(event, params, paywallOverrides, delegate)
+            viewState.value = newView
+        } catch (e: Throwable) {
+            errorState.value = e
+        }
+    }
+
+    when {
+        viewState.value != null -> {
+            // If a paywall is returned, it'll be provided here
+            viewState.value?.let { viewToRender ->
+                AndroidView(
+                    factory = { context ->
+                        viewToRender
+                    }
+                )
+            }
+        }
+        errorState.value != null -> {
+            errorComposable(errorState.value!!)
+        }
+        else -> {
+            loadingComposable()
+        }
+    }
+}
+
