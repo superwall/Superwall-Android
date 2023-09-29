e051e0084d466d32dd8c56c2e6c1ef20530456d4
Brian Anglin <brianranglin@gmail.com>
2023-09-29 15:04:20 -0400

    Change everything to a .kts file

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath("com.karumi:shot:5.14.1")
+    }
+}
+
+plugins {
+    id("com.android.application") version "7.4.2" apply false
+    id("com.android.library") version "7.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.21"
+}
