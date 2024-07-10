5f118dfd929a971f358a2b699344596516cbf4d3
Ian Rumac <ian.rumac@outlook.com>
2024-07-10 12:15:27 +0200

    Add KDoc

--- superwall/src/main/java/com/superwall/sdk/utilities/SuperwallDSL.kt
+++ superwall/src/main/java/com/superwall/sdk/utilities/SuperwallDSL.kt
@@ -20,11 +20,28 @@ class SuperwallBuilder {
     }
 }
 
+/**
+ * Configures a shared instance of [Superwall] for use throughout your app.
+ *
+ * Call this as soon as your app finishes launching in `onCreate` in your `MainApplication`
+ * class.
+ * Check out [Configuring the SDK](https://docs.superwall.com/docs/configuring-the-sdk) for
+ * information about how to configure the SDK.
+ *
+ * @param apiKey Your Public API Key that you can get from the Superwall dashboard
+ * settings. If you don't have an account, you can
+ * [sign up for free](https://superwall.com/sign-up).
+ * @param configure A lambda that allows you to configure the SDK. Inside the configuration block,
+ * you can setup your [PurchaseController], [ActivityProvider], a completion
+ * closure and [SuperwallOptions] via [SuperwallBuilder.options] closure.
+ * @return The configured [Superwall] instance.
+ */
+
 @SuperwallDSL
 fun Application.configureSuperwall(
     apiKey: String,
     configure: SuperwallBuilder.() -> Unit,
-) {
+): Superwall {
     val builder = SuperwallBuilder().apply(configure)
     Superwall.configure(
         this,
@@ -34,13 +51,20 @@ fun Application.configureSuperwall(
         builder.activityProvider,
         builder.completion,
     )
+    return Superwall.instance
 }
 
+/**
+ * Enables you to define [PaywallOptions] via closure.
+ * */
 @SuperwallDSL
 fun SuperwallOptions.paywalls(action: PaywallOptions.() -> Unit) {
     paywalls = this.paywalls.apply(action)
 }
 
+/**
+ * Enables you to define [SuperwallOptions.Logging] via closure.
+ * */
 @SuperwallDSL
 fun SuperwallOptions.logging(action: SuperwallOptions.Logging.() -> Unit) {
     logging = this.logging.apply(action)
