f13bbcdc09dfceee4806992c416fea6fb15cd37d
Ian Rumac <ian.rumac@outlook.com>
2024-07-18 13:15:19 +0200

    Ensure sandbox gets recreated and uses fallback in case of crashing

--- superwall/src/main/java/com/superwall/sdk/paywall/presentation/rule_logic/javascript/SandboxJavascriptEvaluator.kt
+++ superwall/src/main/java/com/superwall/sdk/paywall/presentation/rule_logic/javascript/SandboxJavascriptEvaluator.kt
@@ -1,7 +1,6 @@
 package com.superwall.sdk.paywall.presentation.rule_logic.javascript
 
 import androidx.javascriptengine.JavaScriptSandbox
-import com.superwall.sdk.dependencies.RuleAttributesFactory
 import com.superwall.sdk.logger.LogLevel
 import com.superwall.sdk.logger.LogScope
 import com.superwall.sdk.logger.Logger
@@ -11,39 +10,42 @@ import com.superwall.sdk.models.triggers.UnmatchedRule
 import com.superwall.sdk.paywall.presentation.rule_logic.expression_evaluator.SDKJS
 import com.superwall.sdk.paywall.presentation.rule_logic.tryToMatchOccurrence
 import com.superwall.sdk.storage.Storage
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.guava.await
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 internal class SandboxJavascriptEvaluator(
-    val jsSandbox: JavaScriptSandbox,
-    val factory: RuleAttributesFactory,
-    val storage: Storage,
+    private val jsSandbox: JavaScriptSandbox,
+    private val ioScope: CoroutineScope,
+    private val storage: Storage,
 ) : JavascriptEvaluator {
     override suspend fun evaluate(
         base64params: String,
         rule: TriggerRule,
-    ): TriggerRuleOutcome {
-        val jsIsolate = jsSandbox?.createIsolate()
-        jsIsolate?.addOnTerminatedCallback {
-            Logger.debug(
-                logLevel = LogLevel.error,
-                scope = LogScope.superwallCore,
-                message = "$it",
-            )
+    ): TriggerRuleOutcome =
+        withContext(ioScope.coroutineContext) {
+            val jsIsolate = jsSandbox?.createIsolate()
+            jsIsolate?.addOnTerminatedCallback {
+                Logger.debug(
+                    logLevel = LogLevel.error,
+                    scope = LogScope.superwallCore,
+                    message = "$it",
+                )
+            }
+
+            val resultFuture = jsIsolate?.evaluateJavaScriptAsync("$SDKJS\n $base64params")
+
+            val result = resultFuture?.await()
+            jsIsolate?.close()
+
+            if (result.isNullOrEmpty()) {
+                TriggerRuleOutcome.noMatch(UnmatchedRule.Source.EXPRESSION, rule.experiment.id)
+            } else {
+                val expressionMatched = result == "true"
+                rule.tryToMatchOccurrence(storage, expressionMatched)
+            }
         }
-
-        val resultFuture = jsIsolate?.evaluateJavaScriptAsync("$SDKJS\n $base64params")
-
-        val result = resultFuture?.await()
-        jsIsolate?.close()
-
-        if (result.isNullOrEmpty()) {
-            return TriggerRuleOutcome.noMatch(UnmatchedRule.Source.EXPRESSION, rule.experiment.id)
-        } else {
-            val expressionMatched = result == "true"
-            return rule.tryToMatchOccurrence(storage, expressionMatched)
-        }
-    }
 
     override fun teardown() {
         runBlocking {
