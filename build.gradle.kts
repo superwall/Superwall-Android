79f8a6eab11e28c17a6f87b0ede79a576d78bc07
Ian Rumac <ian.rumac@outlook.com>
2024-09-06 21:33:09 +0200

    Extract compose, add consumer proguard rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.serialization) apply false
+    alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.androidLibrary) apply false
 }
