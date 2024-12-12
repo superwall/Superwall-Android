e597932ebf76d5ee7a843dbccb057456cb5366be
Ian Rumac <ian.rumac@outlook.com>
2024-12-12 17:38:06 +0100

    Remove deprecated naming and cleanup the code

--- superwall/src/main/java/com/superwall/sdk/paywall/view/survey/SurveyPresentationResult.kt
+++
@@ -1,9 +0,0 @@
-package com.superwall.sdk.paywall.view.survey
-
-enum class SurveyPresentationResult(
-    val rawValue: String,
-) {
-    SHOW("show"),
-    HOLDOUT("holdout"),
-    NOSHOW("noShow"),
-}
