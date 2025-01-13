f98997aecce86b3856c46dbd0fbbeb93e3338558
Ian Rumac <ian.rumac@outlook.com>
2025-01-13 14:38:58 -0600

    Prepare basics for web2app

---
+++ superwall/src/main/java/com/superwall/sdk/web/WebPaywallRedeemer.kt
@@ -0,0 +1,55 @@
+package com.superwall.sdk.web
+
+import android.content.Context
+import com.superwall.sdk.Superwall
+import com.superwall.sdk.logger.LogLevel
+import com.superwall.sdk.logger.LogScope
+import com.superwall.sdk.logger.Logger
+import com.superwall.sdk.misc.IOScope
+import com.superwall.sdk.misc.fold
+import com.superwall.sdk.models.entitlements.Entitlement
+import com.superwall.sdk.network.Network
+import com.superwall.sdk.utilities.withErrorTracking
+import kotlinx.coroutines.launch
+
+class WebPaywallRedeemer(
+    val context: Context,
+    val ioScope: IOScope,
+    val deepLinkReferrer: CheckForReferral,
+    val network: Network,
+    val setEntitlementStatus: (List<Entitlement>) -> Unit,
+) {
+    init {
+        ioScope.launch {
+            checkForRefferal()
+        }
+    }
+
+    suspend fun checkForRefferal() =
+        withErrorTracking {
+            deepLinkReferrer
+                .checkForReferral()
+                .fold(
+                    onSuccess = {
+                        redeem(token = it)
+                    },
+                    onFailure = { throw it },
+                )
+        }
+
+    suspend fun redeem(token: String) =
+        network
+            .redeemToken(token, Superwall.instance.userId)
+            .fold({
+                setEntitlementStatus(it.entitlements)
+            }, {
+                Logger.debug(
+                    LogLevel.error,
+                    LogScope.webEntitlements,
+                    "Failed to redeem purchase token",
+                    info = mapOf(),
+                )
+            })
+
+    suspend fun checkForWebEntitlements(userId: String) = network.webEntitlements(userId)
+}
