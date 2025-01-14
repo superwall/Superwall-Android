efa02efd246921fc6b675b1ad7c394b5235b391f
Ian Rumac <ian.rumac@outlook.com>
2025-01-13 19:26:30 -0600

    Create pixel snapshot screenshot tests

--- app/src/androidTest/java/com/example/superapp/test/AlternativeSetupTest.kt
+++ app/src/androidTest/java/com/example/superapp/test/AlternativeSetupTest.kt
@@ -72,7 +72,7 @@ class AlternativeSetupTest {
             )
             paywallPresentsFor(UITestHandler.test53Info)
         }
-
+/*
     @Test
     fun test_paywall_displays_on_deep_link() =
         with(dropshots) {
@@ -86,6 +86,8 @@ class AlternativeSetupTest {
             )
             paywallPresentsFor(UITestHandler.test57Info)
         }
+
+ */
 
     @Test
     fun test_paywall_displays_on_decline() =
