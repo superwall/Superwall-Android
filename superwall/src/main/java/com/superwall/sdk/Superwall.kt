b95aa07c07460c51800407cf36dc2d41380e7d32
Ian Rumac <ian.rumac@outlook.com>
2026-03-12 12:34:17 +0100

    Enable product replacement

--- superwall/src/main/java/com/superwall/sdk/Superwall.kt
+++ superwall/src/main/java/com/superwall/sdk/Superwall.kt
@@ -1337,6 +1337,30 @@ class Superwall(
                         }
                 }
 
+                is PaywallWebEvent.InitiateReplacement -> {
+                    if (purchaseTask != null) {
+                        return@launchWithTracking
+                    }
+                    paywallView.updateState(
+                        PaywallViewState.Updates.SetLoadingState(PaywallLoadingState.LoadingPurchase),
+                    )
+                    purchaseTask =
+                        launch {
+                            try {
+                                dependencyContainer.transactionManager.purchase(
+                                    Internal(
+                                        paywallEvent.productId,
+                                        paywallView.controller.state,
+                                        paywallEvent.replacementMode,
+                                    ),
+                                    shouldDismiss = paywallEvent.shouldDismiss,
+                                )
+                            } finally {
+                                purchaseTask = null
+                            }
+                        }
+                }
+
                 is InitiateRestore -> {
                     dependencyContainer.transactionManager.tryToRestorePurchases(paywallView)
                 }
