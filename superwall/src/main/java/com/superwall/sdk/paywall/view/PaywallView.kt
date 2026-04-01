06d54a91d14eca0caff595fea674f3dfd46b067f
Ian Rumac <ian.rumac@outlook.com>
2026-04-01 11:59:54 +0200

    Add guard to prevent pw from dismissing on deep link

--- superwall/src/main/java/com/superwall/sdk/paywall/view/PaywallView.kt
+++ superwall/src/main/java/com/superwall/sdk/paywall/view/PaywallView.kt
@@ -398,8 +398,8 @@ class PaywallView(
         }
     }
 
-    fun beforeOnDestroy() {
-        if (state.isBrowserViewPresented) {
+    fun beforeOnDestroy(isActivityFinishing: Boolean = false) {
+        if (state.isBrowserViewPresented && !isActivityFinishing) {
             return
         }
         factory.updatePaywallInfo(info)
@@ -408,8 +408,8 @@ class PaywallView(
             .willDismissPaywall(info)
     }
 
-    suspend fun destroyed() {
-        if (state.isBrowserViewPresented) {
+    suspend fun destroyed(isActivityFinishing: Boolean = false) {
+        if (state.isBrowserViewPresented && !isActivityFinishing) {
             return
         }
 
